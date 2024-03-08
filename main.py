f0f6ff78402239c11f26d6d9e3dc8991a4caab6c
Sqfav <igor.mozzy@outlook.com>
2024-03-08 20:06:24 +0100

    Класс Task вынесен из TaskManager; добавлена возможность добавления и просмотра задач пользовательским вводом в консоли

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import logging
 logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+class Task:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+
 class TaskManager:
     def __init__(self):
         self.__tasks = []
 
-    class Task:
-        def __init__(self, title, description):
-            self.title = title
-            self.description = description
-
     def add_task(self, title, description):
-        obj = self.Task(title, description)
+        obj = Task(title, description)
         self.__tasks.append(obj)
         logging.info(f'Task added: {title}; {description}')
 
@@ -25,12 +26,24 @@ class TaskManager:
 
 if __name__ == '__main__':
     manager = TaskManager()
-
+    # предустановленные задачи
     manager.add_task('test task', 'important')
     manager.add_task('check email', 'very important')
     manager.add_task('wake up, Neo...', 'extremely important')
 
-    tasks = manager.get_tasks
-
-    for i, task in enumerate(tasks, start=1):
-        print(f'Task {i}: {task.title} - {task.description}')
+    while True:
+        print('1 - добавить задачу, 2 - показать список задач, 0 - выход')
+        answer = input('Введите команду: ')
+        if answer not in ['0', '1', '2']:
+            print('Недопустимая команда.')
+            continue
+        elif answer == '0':
+            exit()
+        elif answer == '1':
+            title = input('Заголовок задачи: ')
+            description = input('Описание: ')
+            manager.add_task(title, description)
+        elif answer == '2':
+            tasks = manager.get_tasks
+            for i, task in enumerate(tasks, start=1):
+                print(f'Task {i}: {task.title} - {task.description}')
